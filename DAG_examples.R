f137dc373a3812dccd0cf88593861e20831d2470
collazoa <anja.collazo@charite.de>
2021-05-28 16:04:56 +0200

    Update DAG_examples.R

--- DAG_examples.R
+++ DAG_examples.R
@@ -79,5 +79,37 @@ legend2%>%kbl%>%kable_classic(full_width = F)
 
 
 
+#confounder 
+con<- dagitty( "dag {
+  A <-L1 -> L2 -> Y
+  A <-L1 -> L3 -> Y
+  A -> Y
+  A [exposure]
+  Y [outcome]
+  }")
+
+coordinates(con) <-  list(
+  x=c(A=3, Y=5, L1=1, L2 = 2, L3 = 2),
+  y=c(A=5, Y = 5, L1 = 4.5, L2= 4.5, L3 = 4) )
+plot(con)
 
 
+tidy_dagitty(con)%>%
+  mutate(
+    y = c(1,3,3,3,3,4,1 ),
+    yend= c(1,1,3,4,1,1,NA )
+  ) %>%
+  ggplot(aes(x = x, y = y, xend = xend, yend = yend)) +
+  geom_dag_node(color = "gray",
+                alpha = 0.8) +
+  geom_dag_edges(
+    edge_colour = "black",
+    edge_width = .8
+  ) +
+  geom_dag_text(color = "white")+
+  geom_dag_text() +
+  theme_dag()
+
+
+legend_con<-data.frame(variables = c("A", "L1", "L2", "L3", "Y"), legend = c("gene knock-out", "knockout line breeding", "dam caring behaviour", "sensorial dysfunction", "behavioural outcome"))
+legend_con%>%kbl%>%kable_classic(full_width = F)
