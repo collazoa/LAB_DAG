a879672508f9b771ae117b93eb7ee8ae0e49fa23
collazoa <anja.collazo@charite.de>
2023-07-03 21:33:00 +0200

    cleaning repository, update all code for figures, tables

---
+++ manuscript_figures_code/code_fig2.R
@@ -0,0 +1,243 @@
+source("./manuscript_figures_code/load_packages.R")
+# settings for styling visualization
+cols <- RColorBrewer::brewer.pal(6, "Dark2")
+theme_set(theme_classic(base_size = 12))
+
+
+#######################################
+# function to simulate collider bias
+#######################################
+
+sim.dat <- function(N,
+                    b_0,
+                    b_1,
+                    b_2,
+                    g_0,
+                    g_1,
+                    g_2,
+                    sd_w,
+                    sd_y,
+                    cutoff_W) {
+  A <- rep(0:1, each = N / 2)
+  
+  # causal model
+  L <- rnorm(n = 2 * N, mean = 25, sd = 5)
+  W <- g_0 + g_1 * A + g_2 * L + rnorm(N, 0, sd_w)
+  Y <- b_0  + b_1 * A + b_2 * L + rnorm(N, 0, sd_y)
+  S <- W >= quantile(W, probs = cutoff_W)
+  
+  return(data.frame(A, L, Y, W, S))
+}
+
+
+############################################
+# creating a illustrative example dataset
+# for Fig 2 and Fig 3
+############################################
+
+set.seed(290) # reproduces a specific random sample
+
+#########################################################
+# d is the complete data set (corresponding to Approach 1)
+# d_s is the censored data set (corresponding to Approach 2)
+# attrition is moderate (30%), negative side-effects on
+# animal welfare are moderate
+# total sample size chosen is n = 20
+
+
+d <- sim.dat(
+  N = 20,
+  b_0 = 0,
+  b_1 = 0 ,
+  b_2 = 8,
+  g_0 = 0,
+  g_1 = -3,
+  g_2 = -1,
+  sd_w = 2,
+  sd_y = 10,
+  cutoff_W = 0.3
+)
+
+
+d_s <- d %>% filter(S == 1)
+
+#########################################################
+
+
+#########################################################
+#mutating data to prepare for visualization
+d <- d %>%
+  mutate(
+    cols = factor(d$A, labels = c(cols[1], cols[4])),
+    pch = recode(A, `0` = 1, `1` = 17),
+    S1 = factor(d$S, labels = c(0, 1)),
+    bg = recode(S1, `0` = 0.3, `1` = 1),
+    X1 = factor(recode(A, `0` = "Control", `1` = "Drug"))
+  )
+
+d_s <- d_s %>%
+  mutate(
+    cols = factor(d_s$A, labels = c(cols[1], cols[4])),
+    pch = recode(A, `0` = 1, `1` = 17),
+    X1 = factor(recode(A, `0` = "Control", `1` = "Drug"))
+  )
+
+d_summary <- d %>% group_by(A) %>%
+  summarize(
+    mean_L = mean(L),
+    mean_Y = mean(Y),
+    sd_L = sd(L),
+    sd_Y = sd(Y)
+  ) %>%
+  mutate(
+    cols = c(cols[1], cols[4]),
+    xend = c(1.3, 2.3),
+    x = c(0.7, 1.7)
+  )
+
+d_s_summary <- d_s %>% group_by(A) %>%
+  summarize(
+    mean_L = mean(L),
+    mean_Y = mean(Y),
+    sd_L = sd(L),
+    sd_Y = sd(Y)
+  ) %>%
+  mutate(
+    cols = c(cols[1], cols[4]),
+    xend = c(1.3, 2.3),
+    x = c(0.7, 1.7)
+  )
+
+#######################################################
+
+
+
+######################################################
+# visualization for Figure 2
+
+letter <- theme(plot.title = element_text(face = "bold", 
+                                          size = 14), 
+                axis.title = element_text(face = "bold", 
+                                          size = 14), 
+                axis.text = element_text(face = "bold", 
+                                         size = 12), 
+                plot.margin = unit(c(2,2,2,0), "cm"),
+)
+
+
+fig2a <- 
+  ggplot() +
+  geom_quasirandom(
+    data = d,
+    aes(y = L, x = X1, fill = cols),
+    shape = 21,
+    color = "darkblue",
+    alpha = d$bg,
+    size = 5,
+    width = 0.3
+  ) +
+  scale_fill_manual(values = c(cols[1], cols[4]))+
+  geom_segment(
+    data = d_summary,
+    aes(
+      x = x,
+      y = mean_L,
+      yend = mean_L,
+      xend = xend, 
+    ),
+    color = d_summary$cols,
+    linewidth = 1,
+    alpha = 0.3
+  ) +
+  geom_segment(
+    data = d_s_summary,
+    aes(
+      x = x,
+      y = mean_L,
+      yend = mean_L,
+      xend = xend, 
+    ),
+    color = d_summary$cols,
+    linewidth = 1,
+    alpha = 1
+  ) +
+  ylab(expression("Initial infarct size (mm" ^ 3 * ")")) +
+  xlab("") +
+  ylim(10, 40) +
+  ggtitle("A") +
+  letter+
+  theme(legend.position = "none", 
+        plot.margin = unit(c(2,0,2,2), "cm"),
+  )
+
+plot(fig2a)
+
+
+fig2b <- 
+  ggplot() +
+  geom_quasirandom(
+    data = d,
+    aes(y = Y, x = X1, group = X1, fill = cols),
+    shape = 21,
+    color = "darkblue",
+    alpha = d$bg,
+    size = 5,
+    width = 0.3
+  ) +
+  scale_fill_manual(values = c(cols[1], cols[4]))+
+  geom_segment(
+    data = d_summary,
+    aes(
+      x = x,
+      y = mean_Y,
+      yend = mean_Y,
+      xend = xend
+    ),
+    color = d_summary$cols,
+    linewidth = 1,
+    alpha = 0.3
+  ) +
+  geom_segment(
+    data = d_s_summary,
+    aes(
+      x = x,
+      y = mean_Y,
+      yend = mean_Y,
+      xend = xend
+    ),
+    color = d_summary$cols,
+    linewidth = 1,
+    alpha = 1
+  ) +
+  ylab(expression("Final infarct size (mm" ^ 3 * ")")) +
+  xlab("") +
+  ylim(100, 310) +
+  ggtitle("B") +
+  letter + 
+  theme(
+    legend.position = "none",
+    #plot.margin = unit(c(2,2,2,0.5), "cm")
+  )
+
+plot(fig2b)
+
+fig2 <- grid.arrange(
+  
+  fig2a, 
+  fig2b,
+  
+  nrow = 1,
+  ncol = 2
+)
+
+
+fig2
+
+
+# annotate_figure(fig2, 
+#                 top = text_grob("Biased estimates through collider bias stratification", 
+#                                 color = "black", 
+#                                 face = "bold", 
+#                                 size = 20, 
+#                                 just = "right"))
+# ###########################################################
---
+++ manuscript_figures_code/code_fig3_table1.R
@@ -0,0 +1,262 @@
+source("./manuscript_figures_code/load_packages.R")
+
+# settings for styling visualization
+cols <- RColorBrewer::brewer.pal(6, "Dark2")
+
+
+########################################################
+# setting parameter values 
+
+# E Bernoulli prob_A
+# L is normal with mean_L and sd_L
+# Y = b_0 + b_1*E + b_2*L + rnorm(0,sd_Y)
+# W = g_0 + g_1*E + g_2*L + rnorm(0, sd_W)
+
+prob_A = 0.5          # probability of being assigned to treatment or control 
+mean_L = 25           # initial infarct size 
+sd_L = 5              # standard deviation of initial infarct size
+b_0 = 0               # intercept value for final infarct size 
+b_1 = c(0)            # treatment effect - here null-effect 
+b_2 = 8               # effect of initial infarct size on final infarct size 
+g_0 = 3               # intercept value of animal welfare W 
+g_1 = c(-1,-3,-6)     # effect of treatment on animal welfare (E -> W) 
+g_2 = c(-1)           # effect of initial infarct size on welfare (L -> W)
+sd_Y = 10             # standard deviation of final infarct size Y  
+sd_W = 2              # standard deviation of animal welfare W 
+cutoff_W = c(0.2, 0.3,0.5) # attrition rates 
+n = c(10, 20, 50) #total number of animals in the experiment
+
+
+
+# combining all possible parameter values and values of n 
+report <- expand_grid(prob_A, mean_L, 
+                      sd_L, b_0, b_1, 
+                      b_2, g_0, g_1, 
+                      g_2, sd_Y, sd_W, 
+                      cutoff_W, n)
+# setting number of random draws 
+B = 10000
+
+######################################################################
+
+
+######################################################################
+# simulation of B random draws with set parameter values 
+
+########################################################
+
+
+# preparation of empty matrix for model 1, model 2 & model 3 
+# for later storage of values from replications 
+m1 <- matrix(NA_real_, ncol=B, nrow=nrow(report))
+m2 <- matrix(NA_real_, ncol=B, nrow=nrow(report))
+m3 <- matrix(NA_real_, ncol=B, nrow=nrow(report))
+
+
+# ensure reproducibility of random draw 
+set.seed(10000)
+
+######################################################################
+# function for replication draws 
+
+# here starts the counting of the row
+for (i in 1:nrow(report)) {
+  
+  # here starts the loop in b
+  for (b in 1:B) {
+    
+    # create the dataset
+    dat <- data.frame(A = rep(0:1,report$prob_A[i]*report$n[i], each=1),
+                      L = rnorm(n=report$n[i], mean=report$mean_L[i], 
+                                sd=report$sd_L[i]))
+    dat$W <- report$g_0[i] + report$g_1[i]*dat$A + report$g_2[i]*dat$L + rnorm(report$n[i], 0, report$sd_W[i])
+    dat$Y <- report$b_0[i] + report$b_1[i]*dat$A + report$b_2[i]*dat$L + rnorm(report$n[i], 0, report$sd_Y[i])
+    dat$S <- dat$W >= quantile(dat$W, probs=report$cutoff_W[i]) 
+    dat_s <- dat %>% filter(S==TRUE)  					#only selected animals
+    m2[i,b] =  mean(dat_s$Y[dat_s$A==1]) - mean(dat_s$Y[dat_s$A==0]) 	#2- naive, Among selected animals only, mean of the outcome among treated minus mean of outcome among untreated
+    
+    m1[i,b] = mean(dat$Y[dat$A==1]) - mean(dat$Y[dat$A==0])		#1 - unbiased, Among all animals, mean of the outcome among treated minus mean of outcome among untreated
+    
+    model<-lm(Y ~ A + L, data = dat_s)					#regression for outcome only among selected animals, adjusting for confounding (L)
+    m3[i,b]<-model$coefficients[2]					#3 - unbiased, extracted coeff for A (saved)
+  }
+}
+
+################################################################################
+
+# For 27 scenarios (index = scenario)
+# data transformation for stratification and visualization 
+
+m1_tibble <- as_tibble(m1) %>% mutate(model = "m1", 
+                                      index = 1:27)
+m2_tibble <- as_tibble(m2) %>% mutate(model = "m2", 
+                                      index = 1:27)
+m3_tibble <- as_tibble(m3) %>% mutate(model = "m3", 
+                                      index = 1:27)
+
+df <- rbind(m1_tibble, m2_tibble, m3_tibble)
+
+
+###############################################################################
+
+
+load("./manuscript_figures_code/df_fig.RData")
+
+
+
+names<-NULL
+
+for (i in 1:B) {
+  names[i] <- c(paste("estimate_",i, sep = ""))
+}
+colnames(df)<-c(names, "model", "index") 
+
+
+#long format 
+df2 <- gather(df, 
+              key  = sim_number, 
+              value = effect_estimate, 
+              estimate_1:estimate_10000)
+
+df3 <- report %>% 
+  mutate(index = 1:nrow(report))%>%
+  select(c(index,n, cutoff_W, g_1))
+
+df3$g_1 <- factor(df3$g_1,levels = c(-1, -3, -6), 
+                  labels = c("minor", "moderate", "major"))
+df3$cutoff_W <- factor(df3$cutoff_W, levels = c(0.2,0.3,0.5),
+                       labels = c("20", "30", "50"))
+df4 <- inner_join(df2, df3)
+
+
+# missing values 
+df2 %>%
+  group_by(model) %>%
+  summarize(n.sum = n(), 
+            n.na = sum(is.na(effect_estimate)),
+            pct = n.na/n.sum)
+
+na_m2 <- 
+  df2 %>%
+  filter(model == "m2") %>%
+  group_by(index) %>%
+  summarize(n.sum = n(), 
+            n.na = sum(is.na(effect_estimate)), 
+            pct = n.na/n.sum)
+na_m3 <- 
+  df2 %>%
+  filter(model == "m3") %>%
+  group_by(index) %>%
+  summarize(n.sum = n(), 
+            n.na = sum(is.na(effect_estimate)), 
+            pct = n.na/n.sum)  
+
+na_m2 <- 
+  inner_join(na_m2, df3, by = "index")
+
+na_m3 <- 
+  inner_join(na_m3, df3, by = "index")
+
+
+###################################################
+#
+#   effect estimates for different values of n, 
+#   attrition frequency and side effects 
+#
+##################################################
+
+
+
+r4<- df4%>%
+  filter(model == "m2")%>%
+  group_by(g_1, cutoff_W)%>%
+  summarize(mean_bias = round(mean(effect_estimate, na.rm = TRUE),1),
+            quan_25 = round(quantile(effect_estimate, probs = 0.025, na.rm = TRUE),1),
+            quan_975 = round(quantile(effect_estimate, probs = 0.975, na.rm = TRUE),1))
+
+r5<- df4%>%
+  filter(model == "m2")%>%
+  group_by(g_1, n)%>%
+  summarize(mean_bias = round(mean(effect_estimate, na.rm = TRUE),1),
+            quan_25 = round(quantile(effect_estimate, probs = 0.025, na.rm = TRUE),1),
+            quan_975 = round(quantile(effect_estimate, probs = 0.975, na.rm = TRUE),1)) %>%
+  arrange(n)
+
+r5
+
+r6<- df4%>%
+  filter(model == "m2")%>%
+  group_by(cutoff_W, n)%>%
+  summarize(mean_bias = round(mean(effect_estimate, na.rm = TRUE),1),
+            quan_25 = round(quantile(effect_estimate, probs = 0.025, na.rm = TRUE),1),
+            quan_975 = round(quantile(effect_estimate, probs = 0.975, na.rm = TRUE),1)) %>%
+  arrange(n)
+r6
+
+
+r7<- df4%>%
+  filter(model == "m2")%>%
+  group_by(g_1, cutoff_W, n)%>%
+  summarize(mean_bias = round(mean(effect_estimate, na.rm = TRUE),1),
+            quan_25 = round(quantile(effect_estimate, probs = 0.025, na.rm = TRUE),1),
+            quan_975 = round(quantile(effect_estimate, probs = 0.975, na.rm = TRUE),1)) %>%
+  arrange(n)
+r7
+
+
+
+###########################################################################################
+# Visualization 
+letter <- theme(plot.title = element_text(face = "bold", 
+                                          size = 20, 
+                                          margin = margin(2,0,0.5,0, unit = "cm")), 
+                axis.text = element_text(face = "bold", 
+                                         size = 12),
+                axis.title.y = element_text(face = "bold", 
+                                         size = 14, 
+                                         margin = margin(0,0.5,0,2, unit = "cm")),
+                axis.title.x = element_text(face = "bold", 
+                                           size = 14, 
+                                           margin = margin(0.5,0,0.5,0, unit = "cm")),
+                plot.margin = unit(c(2,2,2,0), "cm"),
+                panel.grid.major = element_blank(),
+                legend.position = "bottom", 
+                legend.title = element_blank(),
+                legend.text = element_text(size = 12, 
+                                           face = "bold"),
+                strip.text = element_text(size = 12, 
+                                          face = "bold"), 
+                strip.background = element_rect(fill = "snow2"))
+
+
+
+
+# Figure 3  violin plot 
+
+df4$n <- as.factor(df4$n)
+levels(df4$n) <- c(expression("n = 10"), expression("n = 20"), expression("n = 50"))
+levels(df4$cutoff_W) <- c("attrition frequency = 20%", "attrition frequency = 30%", "attrition frequency = 50%")
+
+
+fig3<- ggplot(na.omit(df4))+
+  geom_hline(aes(yintercept = 0), linetype = "dashed")+
+  geom_violin(aes(x = g_1, y = effect_estimate, fill = model), alpha = 0.7)+
+  geom_vline(xintercept = c(1.5, 2.5), 
+             linewidth = 0.8)+
+  facet_grid( n ~ cutoff_W)+
+  coord_cartesian(ylim = c(-40,40))+
+  scale_fill_manual(values = c(cols[1], cols[4], cols[8]), 
+                    labels = c("oracle", "naive", "adjusted"))+
+  labs(#title = "Adjustment for initial infarct size mitigates collider stratification bias",
+       y = "estimated effect", 
+       x = "strength of negative side-effect of treatment on welfare")+
+  theme_bw()+
+  letter
+
+fig3
+
+
+
+
+
+
